4d048fab6af69973f31a936dc39be6fa53211ebd
westinyang <517858177@qq.com>
2023-04-09 22:33:19 +0800

    feat: 代码头添加许可声明，应用信息模型优化，应用列表布局组件化

--- entry/src/main/ets/abilitystage/MyAbilityStage.ts
+++ entry/src/main/ets/abilitystage/MyAbilityStage.ts
@@ -1,9 +1,25 @@
+/*
+ * Copyright (C) 2023 westinyang https://gitee.com/ohos-dev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import AbilityStage from '@ohos.app.ability.AbilityStage';
 
 export default class MyAbilityStage extends AbilityStage {
     onCreate() {
         // 应用的HAP在首次加载的时，为该Module初始化操作
-        console.log("MyAbilityStage onCreate");
     }
     onAcceptWant(want) {
         // 仅specified模式下触发
