bb91e76a17767ddcce8aac643f6d19eb8850b239
westinyang <517858177@qq.com>
2023-04-10 11:18:36 +0800

    feat: 实现应用下载、安装、打开、检测是否存在、获取MainAbility

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -29,6 +29,8 @@ export default class EntryAbility extends UIAbility {
     }
 
     onWindowStageCreate(windowStage: window.WindowStage) {
+        globalThis.abilityContext = this.context;
+
         // Main window is created, set main page for this ability
         hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
